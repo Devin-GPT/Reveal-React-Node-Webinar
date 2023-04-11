065d1063168bea4f67c726440dd076e0ad77fe8b
Jason Beres <jasonberes@gmail.com>
2023-04-10 21:50:59 -0400

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import {Route,Routes} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import CreateDashboard from './components/CreateDashboard';
+import ViewDashboard from './components/ViewDashboard';
+
+function App() {
+  return (
+    <div className='border' >
+    { <Header /> }
+    <Routes>
+      <Route path='/create' Component={CreateDashboard}/>
+      <Route path='/view' Component={ViewDashboard}/>
+      <Route path='/' Component={ViewDashboard}/> 
+    </Routes>
+    </div>
+    );
+}
+
+export default App;
